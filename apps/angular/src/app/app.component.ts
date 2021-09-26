e2bffc1ad6396f84323db37dc7dac8b9fb223fdb
Wahaj Ahmed Khan <wahajahmedkhan95@gmail.com>
2021-09-27 04:12:34 +0500

    initial setup

---
+++ apps/angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'type-script-monorepo-setup-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular';
+}
